7248ff99e134cc8283c847713fcecdef262c9563
BT_1N3_26 <sample@bths.edu>
2025-01-14 13:20:39 -0500

    th-4 completed - zaif

--- src/Hunter.java
+++ src/Hunter.java
@@ -18,7 +18,7 @@ public class Hunter {
      */
     public Hunter(String hunterName, int startingGold) {
         this.hunterName = hunterName;
-        kit = new String[5]; // only 5 possible items can be stored in kit
+        kit = new String[6]; // only 5 possible items can be stored in kit
         gold = startingGold;
     }
 
@@ -141,6 +141,7 @@ public class Hunter {
         addItem("machete");
         addItem("horse");
         addItem("boat");
+        addItem("boots");
     }
     /**
      * @return A string representation of the hunter.
